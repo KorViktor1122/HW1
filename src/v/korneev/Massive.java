95f7a4990699f387b54b90b8a2662365c44351f6
Viktor Korneev <korneev1122@gmail.com>
2020-02-24 11:01:31 +0300

    Commint№1

--- src/v/korneev/Massive.java
+++ src/v/korneev/Massive.java
@@ -24,34 +24,7 @@ public class Massive {
 
         }
 
-
-
     }
-
-/*
-    private Object human;
-    private Object Cat;
-    private Object Robot;
-    private Object runlength;
-    private Object wallhiegth;
-
-   public int[] participant = {(int) human, (int) Cat, (int) Robot};
-
-   public int[] block = {(int) runlength, (int) wallhiegth};
-
-
-    public Massive(Object human, Object cat, Object robot, Object runlength, Object wallhiegth) {
-        this.human = human;
-        Cat = cat;
-        Robot = robot;
-        this.runlength = runlength;
-        this.wallhiegth = wallhiegth;
-    }
-
-    public static void main(String[] args) {
-
-    }*/
-
 }
 
 
